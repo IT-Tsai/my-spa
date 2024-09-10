e4211fa560b8c2330e954e83ff918a5419c3eeb7
IT-Tsai <it.tsai.1113@gmail.com>
2024-09-10 09:00:55 +0800

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/my-spa",
+  //basePath: "/my-spa",
   output: "export", // <=== enables static exports
+  distDir: "dist",
   reactStrictMode: true,
+  //assetPrefix: "/my-spa/", // This prefix is needed to correctly load assets from the specified base path
+  trailingSlash: true, // Ensures trailing slashes are added to all paths (helps with static site generation)
 };
 
 export default nextConfig;
