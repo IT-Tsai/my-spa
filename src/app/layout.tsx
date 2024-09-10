b82d30646ac18507bc4da321889c045ce91123b3
IT-Tsai <it.tsai.1113@gmail.com>
2024-09-10 10:44:47 +0800

    update pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={classes.container}>
+      <body className={classes["layout-container"]}>
         <Header />
         {children}
       </body>
