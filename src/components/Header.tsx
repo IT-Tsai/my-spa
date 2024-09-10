aa18df3b056792c899e3f5591485d74cce8b65d4
IT-Tsai <it.tsai.1113@gmail.com>
2024-09-10 10:48:24 +0800

    update header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import classes from "./Header.module.css";
 const Header = () => {
   return <div className={classes.container}>
     <div className={classes.left}>
-      <Link className={classes.link} href="/home">Home</Link>
+      <Link className={classes.link} href="/">Home</Link>
       <Link className={classes.link} href="/experience">Experience</Link>
     </div>
   </div>
